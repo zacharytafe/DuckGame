b458e69aef941d9c5d59783a0a0f3b60ccd61109
zacharytafe <36681233+zacharytafe@users.noreply.github.com>
2020-02-19 15:41:44 +1100

    Edited scene and scripts.

---
+++ Assets/Scripts/DuckScripts/DuckBomb.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace DuckShoot
+{
+    public class DuckBomb : MonoBehaviour
+    {
+        public GameObject bomb;
+        public float pawnRate = 1;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        void OnTriggerEnter(Collider other)
+        {
+            if (other.gameObject.name == "BombPoint")
+            {
+                for (int i = 0; i < 1; i++)
+                {
+                    Instantiate(bomb, new Vector3(i * 0, 0, 0), Quaternion.identity);
+                }
+            }
+        }
+    }
+}
