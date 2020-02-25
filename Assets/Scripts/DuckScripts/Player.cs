efbce5ec1d79cffc4d194f6dcff0d1d17622f702
zacharytafe <36681233+zacharytafe@users.noreply.github.com>
2020-02-25 13:33:21 +1100

    Added health and damage.

--- Assets/Scripts/DuckScripts/Player.cs
+++ Assets/Scripts/DuckScripts/Player.cs
@@ -1,18 +1,50 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class Player : MonoBehaviour
+namespace DuckShoot
 {
-    // Start is called before the first frame update
-    void Start()
+    public class Player : MonoBehaviour
     {
-        
-    }
+        private float startHealth = 100;
+        public float currentHealth;
+        public float damageTake = 10;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        public Text healthCount;
+        public GameObject deathTest;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            currentHealth = startHealth;
+            healthCount.text = currentHealth.ToString();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if(currentHealth <= 0)
+            {
+                Death();
+            }
+        }
+
+        private void OnCollisionEnter(Collision collision)
+        {
+            if(collision.gameObject.tag == "Projectile")
+            {
+                currentHealth -= damageTake;
+                healthCount.text = currentHealth.ToString();
+                Destroy(collision.gameObject);
+            }
+            
+        }
+
+        void Death()
+        {
+            Time.timeScale = 0;
+            deathTest.SetActive(true);
+        }
     }
 }
